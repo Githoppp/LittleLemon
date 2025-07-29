186048c4808c63bc64297c429d2e38ea9f0c7770
Rustom <rustom.m@somaiya.edu>
2025-07-28 20:33:31 -0400

    Made the databases with the models

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'littlelemon_root',  # root isn't used because the extension uses and older authentication method and I had to make a new user(using the older authentication method)
+        'PASSWORD': '475sqL#$!Ak77',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
